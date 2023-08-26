4ca6f1ec04cb0c6db3801782c8f3eede606c5c60
Камиль Мирихан <samgold@MacBook-Air-Kamil.local>
2023-08-26 19:27:36 +0600

    files await

--- src/data/authorsImage.js
+++
@@ -1,9 +0,0 @@
-export const authorImages = {
- Камиль: "/kamil.png",
- SAW: "/saw.jpeg",
- Edward: "/bob.jpeg",
- Арина: "/arina.jpg",
- GPT: "/gpt.png",
- BKAA: "/aa.png",
- Программный: '/12.png',
-};
